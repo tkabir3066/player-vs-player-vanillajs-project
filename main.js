c34ff8f25ecba1104beb924c5ac0bf6dfe8c1256
Tutul Kabir <tutulkabir@gmail.com>
2023-01-15 20:05:54 +0530

    DOM elemment selection

---
+++ main.js
@@ -0,0 +1,9 @@
+// selection
+const formElm = document.querySelector("form");
+const inputElm = document.querySelector("#luck-input");
+const winnerElm = document.querySelector(".winner");
+const winningScoreElm = document.querySelector(".lucky-number");
+const p1ScoreElm = document.querySelector(".p1");
+const p2ScoreElm = document.querySelector(".p2");
+const p1ButtonElm = document.querySelector(".p1Btn");
+const p2ButtonElm = document.querySelector(".p2Btn");
