f6c0e6cb26260a70ce4651f7657effe1a29913a6
Tutul Kabir <tutulkabir@gmail.com>
2023-01-19 18:49:13 +0530

    updated main.js file with selection of DOM element

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("hello");
+// DOM element selection
+
+const formElm = document.querySelector("form");
+const winnerElm = document.querySelector(".winner");
+const winScoreElm = document.querySelector(".lucky-number span");
+const p1ScoreElm = document.querySelector(".p1");
+const p2ScoreElm = document.querySelector(".p2");
+const p1BtnElm = document.querySelector(".p1Btn");
+const p2BtnElm = document.querySelector(".p2Btn");
+const resetBtnElm = document.querySelector("#resetBtn");
